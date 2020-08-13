8ea58e77ce0adf2baf0426c0901868e024d3830f
debbysa <deby.silvia23@gmail.com>
2020-08-13 11:28:42 +0700

    feat(server): initiate server

---
+++ src/coba.ts
@@ -0,0 +1,13 @@
+let panjang: number = 20;
+let lebar: number = 5;
+
+// console.log("Luas persegi panjang = ", panjang * lebar);
+
+function showLuasPersegiPanjang(panjang: number, lebar: number): number {
+  return panjang * lebar;
+}
+
+console.log(
+  "luas persegi panjangku adalah = ",
+  showLuasPersegiPanjang(panjang, lebar)
+);
---
+++ src/server.ts
@@ -0,0 +1,8 @@
+import app from "./app";
+
+const port = 3000;
+
+// koneksi yang dibutuhkan untuk server ada disini
+app.listen(port, () => {
+  console.log(`server started at http://localhost:${port}/`);
+});
